fcd6a96aca4b51575ec4353bc40bf62b8c024e98
Isaell Kelson <isaellkelson@gmail.com>
2023-07-26 21:20:53 -0300

    commit calculadora repeticao

---
+++ logica-de-programação/CalculadoraRepeticao/index.js
@@ -0,0 +1,32 @@
+while (true) {
+    let op = prompt("Digite a operação que deseja fazer\n1: somar \n2: subtrair \n3: multiplicar \n4: dividir \n0: sair")
+
+    if (op == "0") {
+        alert("Saindo da calculadora!")
+        break;    
+    } else if (op === "1" || op === "2" || op === "3" || op === "4") {  
+
+
+        let n1 = prompt("Digite o primeiro número")
+        let n2 = prompt("Digite o segundo número")
+
+        n1 = parseFloat(n1)
+        n2 = parseFloat(n2)
+
+        if (op === "1") {
+            let resultado = alert(`A soma é: ${n1 + n2}`)
+        }
+        else if (op === "2") {
+            let resultado = alert(`A subtração é: ${n1 - n2}`)
+        }
+        else if (op === "3") {
+            let resultado = alert(`A multiplicação é: ${n1 * n2}`)
+        }
+        else if (op ==="4") {
+            let resultado = alert(`A divisão é: ${n1 / n2}`)
+        }
+    }   else {
+            alert("Opção inválida")
+        }
+    
+}
