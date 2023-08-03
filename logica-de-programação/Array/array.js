8dd1deba98fb2b6ac1a58c870d8502d971f3b161
Isaell Kelson <isaellkelson@gmail.com>
2023-08-03 09:14:00 -0300

    Create array.js

---
+++ logica-de-programação/Array/array.js
@@ -0,0 +1,16 @@
+const nomes = ['João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Laura', 'Carlos', 'Mariana', 'Paulo', 'Gabriela'];
+const idades = [25, 30, 22, 28, 35, 29, 31, 26, 24, 27];
+const coresFavoritas = ['azul', 'verde', 'amarelo', 'rosa', 'vermelho', 'laranja', 'roxo', 'cinza', 'preto', 'branco'];
+
+
+console.log('Lista de nomes:', nomes);
+console.log('Lista de idades:', idades);
+console.log('Lista de cores favoritas:', coresFavoritas);
+
+idades[2] = 23;
+coresFavoritas[5] = 'violeta';
+
+
+console.log('Lista de nomes atualizada:', nomes);
+console.log('Lista de idades atualizada:', idades);
+console.log('Lista de cores favoritas atualizada:', coresFavoritas);
