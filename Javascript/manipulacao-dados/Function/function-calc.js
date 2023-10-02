1a11a6dc34d6bfa1c6df305824883ad6b57b5c10
Isaell Kelson <isaellkelson@gmail.com>
2023-10-02 10:53:43 -0300

    commit function

---
+++ Javascript/manipulacao-dados/Function/function-calc.js
@@ -0,0 +1,9 @@
+console.log("Calculadora Simples:");
+const n1 = 10;
+const n2 = 5;
+
+const resultadoSoma = somar(n1, n2);
+console.log(`Soma: ${n1} + ${n2} = ${resultadoSoma}`);
+
+const resultadoSubtracao = subtrair(n1, n2);
+console.log(`Subtração: ${n1} - ${n2} = ${resultadoSubtracao}`);
